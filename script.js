8ba6ec7c5504bf43eaadaf07fdd976030fe4479e
Tite Lunda <98431708+TiteLunda@users.noreply.github.com>
2023-05-03 20:44:28 +0300

    Add of Javascript Function

--- script.js
+++ script.js
@@ -86,3 +86,48 @@ document.querySelector('#form').addEventListener('submit', (e) => {
 document.querySelector('#books').addEventListener('click', (e) => {
   UI.deleteBook(e.target);
 });
+
+
+const list = document.querySelector('#list');
+const addNew = document.querySelector('.nav-item2');
+const contact = document.querySelector('#contact');
+const books = document.querySelector('#books');
+const form = document.querySelector('#form');
+const awesome = document.querySelector('.awesome');
+const hr = document.querySelector('.book-new');
+const contactSection = document.querySelector('.contact');
+
+form.classList.add('hidden');
+contactSection.classList.add('hidden');
+list.style.textDecoration = ('underline');
+
+list.addEventListener('click', () => {
+  list.style.textDecoration = ('underline');
+  addNew.style.textDecoration = ('none');
+  contact.style.textDecoration = ('none');
+  form.classList.add('hidden');
+  books.classList.remove('hidden');
+  contactSection.classList.add('hidden');
+  awesome.classList.remove('hidden');
+});
+
+addNew.addEventListener('click', () => {
+  addNew.style.textDecoration = ('underline');
+  list.style.textDecoration = ('none');
+  contact.style.textDecoration = ('none');
+  form.classList.remove('hidden');
+  books.classList.add('hidden');
+  contactSection.classList.add('hidden');
+  awesome.classList.add('hidden');
+  hr.classList.add('hidden');
+});
+
+contact.addEventListener('click', () => {
+  addNew.style.textDecoration = ('none');
+  list.style.textDecoration = ('none');
+  contact.style.textDecoration = ('underline');
+  form.classList.add('hidden');
+  books.classList.add('hidden');
+  awesome.classList.add('hidden');
+  contactSection.classList.remove('hidden');
+});
